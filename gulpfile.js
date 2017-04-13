bde6e2c90c342fd3d8e733d407a36e978533075f
jacobpetersen <jacob@jacob-petersen.com>
2017-04-12 22:13:49 -0500

    Adding gulp file to minify js and css

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('default', function(callback) {
+    runSequence('clean', ['minifyJS', 'minifyCSS'], callback)
+});
+
+gulp.task('clean', function() {
+    del.sync('min');
+});
+
+gulp.task('minifyJS', function() {
+    return gulp.src('clickRipple.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('min'));
+});
+
+gulp.task('minifyCSS', function() {
+    return gulp.src('clickRipple.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('min'));
+});
